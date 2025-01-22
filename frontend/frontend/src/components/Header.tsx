1a0ebea94867718aee87ef19bb35a7a74c51cead
gampalachalani <gampalachalanibiseka@gmail.com>
2025-01-22 09:53:48 +0530

    header.tsx

--- frontend/frontend/src/components/Header.tsx
+++ frontend/frontend/src/components/Header.tsx
@@ -5,13 +5,23 @@ const Header: React.FC = () => {
   return (
     <nav className="navbar">
       <div className="container">
+      <div className="left-links">
         <a className="nav-link" href="/home">Home</a>
         <a className="nav-link" href="/entrepreneurs">Entrepreneurs</a>
         <a className="nav-link" href="/investors">Investors</a>
+      </div>
+
+        <div className="search-bar">
+          <input type="text" placeholder="Search..." />
+        </div>
+
+        <div className="right-links">
         <a className="nav-link" href="/funding">Funds</a>
         <a className="nav-link" href="/login">Sign In</a>
-        <a className="nav-link" href="/signup">Sign Up</a>
+        <a className="nav-link signup-btn" href="/signup">Sign Up</a>
       </div>
+    
+    </div>
     </nav>
   );
 };
