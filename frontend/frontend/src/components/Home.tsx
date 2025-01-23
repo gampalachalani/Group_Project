efdb8e7515933a964f4d3e3159bdfd56c0a6a3b9
gampalachalani <gampalachalanibiseka@gmail.com>
2025-01-23 09:03:08 +0530

    form

--- frontend/frontend/src/components/Home.tsx
+++ frontend/frontend/src/components/Home.tsx
@@ -8,8 +8,12 @@ const Home : React.FC = () => {
         <section className="main">
           <h1>Welcome to SeedLink</h1>
           <div className="buttons">
-            <button className="btn btn_1">Join as an Entrepreneur</button>
-            <button className="btn btn_2">Join as an Investor</button>
+               <a href="/entrepreneurform">
+               <button className="btn btn_1">Join as an Entrepreneur</button>
+               </a>
+               <a href="/investorform">
+               <button className="btn btn_2">Join as an Investor</button>
+               </a>
           </div>
         </section>
         <section className="features">
