1b3be0825d9f3cae79d3935c3a78c7f40e7f4172
gampalachalani <gampalachalanibiseka@gmail.com>
2025-01-31 11:23:23 +0530

    forms

---
+++ frontend/frontend/src/components/Entreform.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Entreform : React.FC = () => {
+  return (
+    <div>
+      <h2>Entrepreneur Form</h2>
+      </div>
+  );
+};
+
+export default Entreform;
---
+++ frontend/frontend/src/components/Invesform.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Invesform : React.FC = () => {
+  return (
+    <div>
+      <h2>Investor Form</h2>
+      </div>
+  );
+};
+
+export default Invesform;
