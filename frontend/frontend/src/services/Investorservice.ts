eb68c763031977c64cef9bd0f5f2685048561391
gampalachalani <gampalachalanibiseka@gmail.com>
2025-02-03 08:54:33 +0530

    invest

---
+++ frontend/frontend/src/services/Investorservice.ts
@@ -0,0 +1,61 @@
+import axios from "axios";
+import { InvestFormData } from "../interfaces/investformdata";
+
+const API_URL = "http://localhost:8080/api/investment";
+
+export const submitInvestorForm = async (
+    formData: InvestFormData,
+    userId: string,
+    navigate: (path: string) => void
+  ): Promise<void> => {
+    const data = new FormData();
+    data.append(
+      "investment",
+      JSON.stringify({
+        userId: userId,
+        investorName: formData.investorName,
+        investorJob: formData.investorJob,
+        investorInterest: formData.investorInterest,
+        otherDetails: formData.otherDetails,
+        budgetLimit: formData.budgetLimit,
+        address: formData.address,
+        telNumber: formData.telNumber,
+      })
+    );
+  
+    if (formData.imageFile) {
+      data.append("imageFile", formData.imageFile);
+    }
+  
+    try {
+      await axios.post(`${API_URL}/add`, data, {
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+      navigate("/");
+    } catch (error) {
+      console.error("Error submitting data", error);
+      alert("Failed to submit investor data.");
+    }
+};
+
+export const getAllInvesters = async () => {
+  try {
+    const response = await fetch(`${API_URL}/getAllInvestors`);
+    if (!response.ok) throw new Error("Failed to fetch invester");
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching enterprises:", error);
+    return [];
+  }
+};
+
+export const getInvestorById = async (investorId: string) => {
+  try {
+    const response = await fetch(`${API_URL}/getInvestmentByInvestmentId/${investorId}`);
+    if (!response.ok) throw new Error("Failed to fetch investor");
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching investors:", error);
+    return [];
+  }
+};
