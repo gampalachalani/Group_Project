286c8f4c84374f5abe37352710a1fa77f6f97d79
Nim2001 <nimashajayakodyd@gmail.com>
2025-02-08 07:26:37 +0530

    Update footer

--- frontend/frontend/src/components/Footer.tsx
+++ frontend/frontend/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer: React.FC = () => {
  return (
- <footer className="bg-dark text-center text-white mt-auto py3">
+ <footer className="bg-dark text-center text-white mt-auto py3 sticky bottom-0">
  <div>© 2024 Seed Link</div>
  </footer>
  );
